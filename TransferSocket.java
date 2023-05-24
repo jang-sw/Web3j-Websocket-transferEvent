f7a78538497742bec27d0747cacbf1ce66269601
jang-sw <jsw118536@gmail.com>
2023-05-24 10:07:43 +0900

    Update TransferSocket.java

--- TransferSocket.java
+++ TransferSocket.java
@@ -1,7 +1,9 @@
 package web3;
 
 import java.net.ConnectException;
+import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -23,82 +25,78 @@ public class TransferSocket {
 
 	public static void main(String[] args) {
 		try (Scanner scanner = new Scanner(System.in);){
-			
 //			Set Infura Endpoint & Contract Address
-        	System.out.println("Enter Your Infura WSS Endpoint (  wss://{} ) : ");
-    		String infuraEndpoint = scanner.nextLine();
-    		System.out.println("Enter Your Smart Contract Address : ");
-    		String contractAddress = scanner.nextLine();
+        		System.out.println("Enter Your Infura WSS Endpoint (  wss://{} ) : ");
+			String infuraEndpoint = scanner.nextLine();
+			System.out.println("Enter Your Smart Contract Address : ");
+			String contractAddress = scanner.nextLine();
     		
 //			Try Socket Connect 
-            WebSocketService webSocketService = new WebSocketService(infuraEndpoint, true);
-            System.out.println("== Socket Connecting Started ==");
-            int failCnt = 5;
-            while (failCnt >= 0) {
-            	try {
-        			webSocketService.connect();
-        			break;
-        		} catch (ConnectException e1) {
-        			if(failCnt == 0) e1.printStackTrace();
-        			failCnt--;
-        		}
-            }
-            System.out.println("== Socket Connected ==");
-            
+			WebSocketService webSocketService = new WebSocketService(infuraEndpoint, true);
+			System.out.println("== Socket Connecting Started ==");
+			int failCnt = 5;
+			while (failCnt >= 0) {
+				try {
+					webSocketService.connect();
+					break;
+				} catch (ConnectException e1) {
+					if(failCnt == 0) e1.printStackTrace();
+					failCnt--;
+				}
+            		}
+            		System.out.println("== Socket Connected ==");
+			
 //			The schedule for checking the Web3 Client version has been set to maintain the socket connection.
-            Timer timer = new Timer();
-            TimerTask timerTask = new TimerTask() {
-    			@Override
-    			public void run() {
-    				Request<?, Web3ClientVersion> request = new Request<>("web3_clientVersion", Arrays.asList(), webSocketService, Web3ClientVersion.class);
-    				webSocketService.sendAsync(request, Web3ClientVersion.class).thenAccept(v -> {
-    					LocalDateTime now = LocalDateTime.now();
-    					System.out.println(String.format("[ %s ] [Check Version] - %s", now.toString() ,v.getResult()));
-    				});
-    			}
-    		};
-    		timer.scheduleAtFixedRate(timerTask, 0, 3000000);
+			Timer timer = new Timer();
+			TimerTask timerTask = new TimerTask() {
+    				@Override
+    				public void run() {
+    					Request<?, Web3ClientVersion> request = new Request<>("web3_clientVersion", Arrays.asList(), webSocketService, Web3ClientVersion.class);
+    					webSocketService.sendAsync(request, Web3ClientVersion.class).thenAccept(v -> {
+    						LocalDateTime now = LocalDateTime.now();
+    						System.out.println(String.format("[ %s ] [Check Version] - %s", now.toString() ,v.getResult()));
+    					});
+    				}
+    			};
+    			timer.scheduleAtFixedRate(timerTask, 0, 3000000);
             
 //			Event and filter setup for subscribing to Transfer.
 // 			To examine the transfer logs of all blocks from the initial execution, set the filter's block range from EARLIEST to LATEST.
 
-            Web3j web3 = Web3j.build(webSocketService);  
-            EthFilter filter = new EthFilter(
-                    DefaultBlockParameterName.LATEST,
-                    DefaultBlockParameterName.LATEST,
-                    contractAddress
-            );
-            Event TRANSFER_EVENT = new Event("Transfer", 
-            	    Arrays.<TypeReference<?>>asList(new TypeReference<Address>(true) {}, new TypeReference<Address>(true) {}, new TypeReference<Uint256>(true) {}));
+			Web3j web3 = Web3j.build(webSocketService);  
+			EthFilter filter = new EthFilter(
+				DefaultBlockParameterName.LATEST,
+				DefaultBlockParameterName.LATEST,
+				contractAddress
+			);
+            		Event TRANSFER_EVENT = new Event("Transfer", 
+            	    		Arrays.<TypeReference<?>>asList(new TypeReference<Address>(true) {}, new TypeReference<Address>(true) {}, new TypeReference<Uint256>(true) {}));
             
 //			Add Transfer Event Topic To Filter
-            filter.addSingleTopic(EventEncoder.encode(TRANSFER_EVENT));
+            		filter.addSingleTopic(EventEncoder.encode(TRANSFER_EVENT));
 
 //			Event Subscribe
-            web3.ethLogFlowable(filter).subscribe(eventLog -> {
+			web3.ethLogFlowable(filter).subscribe(eventLog -> {
 //				Event Log analysis
-				
-//            	String smartContractAddress = eventLog.getTopics().get(0);
-//            	String from = eventLog.getTopics().get(1);
-            	String to = eventLog.getTopics().get(2);
-            	String tokenId = eventLog.getTopics().get(3);
-            	LocalDateTime now = LocalDateTime.now();
-            	System.out.println("New Owner :: 0x" + Keys.toChecksumAddress(to).substring(26) + "  &  TokenId :: " + Long.decode(tokenId) + " :: " + now);          
-            }, e -> {
+
+//            			String smartContractAddress = eventLog.getTopics().get(0);
+//            			String from = eventLog.getTopics().get(1);
+				String to = eventLog.getTopics().get(2);
+				String tokenId = eventLog.getTopics().get(3);
+				LocalDateTime now = LocalDateTime.now();
+            			System.out.println("New Owner :: 0x" + Keys.toChecksumAddress(to).substring(26) + "  &  TokenId :: " + Long.decode(tokenId) + " :: " + now);          
+            		}, e -> {
 //				TODO
-            	if (e instanceof ArrayIndexOutOfBoundsException) {
-                    e.printStackTrace();
-                } else if (e instanceof NumberFormatException ) {
-                    e.printStackTrace();
-                } else {
-                    e.printStackTrace();
-                }
-            });
-        } catch {
+				if (e instanceof ArrayIndexOutOfBoundsException) {
+				    e.printStackTrace();
+				} else if (e instanceof NumberFormatException ) {
+				    e.printStackTrace();
+				} else {
+				    e.printStackTrace();
+				}
+			});
+		} catch (Exception e2) {
 //			TODO
 		}
-
     }
-
-
 }
