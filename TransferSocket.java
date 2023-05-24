dec77bab20a4e5c0cbc175f501080af15fbe656c
jang-sw <jsw118536@gmail.com>
2023-05-24 10:00:18 +0900

    Add files via upload

--- TransferSocket.java
+++ TransferSocket.java
@@ -22,64 +22,82 @@ public class TransferSocket {
 	
 
 	public static void main(String[] args) {
-        String infuraEndpoint = "wss:// {Infura WSS Endpoint} ";
-        WebSocketService webSocketService = new WebSocketService(infuraEndpoint, true);
-        System.out.println("== Sokcet Connecting Started ==");
-        int failCnt = 5;
-        while (failCnt >= 0) {
-        	try {
-    			webSocketService.connect();
-    			break;
-    		} catch (ConnectException e1) {
-    			if(failCnt == 0) e1.printStackTrace();
-    			failCnt--;
-    		}
-        }
-        System.out.println("== Sokcet Connected ==");
-        
-//        Timer timer = new Timer();
-//        TimerTask timerTask = new TimerTask() {
-//			@Override
-//			public void run() {
-//				Request<?, Web3ClientVersion> request = new Request<>("web3_clientVersion", Arrays.asList(), webSocketService, Web3ClientVersion.class);
-//				webSocketService.sendAsync(request, Web3ClientVersion.class);
-//			}
-//		};
-//		timer.scheduleAtFixedRate(timerTask, 0, 3000000);
-		
-        
-        Web3j web3 = Web3j.build(webSocketService);
-        
-        String contractAddress = " { Smart Contract Address } ";
-
-        EthFilter filter = new EthFilter(
-                DefaultBlockParameterName.LATEST,
-                DefaultBlockParameterName.LATEST,
-                contractAddress
-        );
-        Event transferEvent = new Event("Transfer", 
-        	    Arrays.<TypeReference<?>>asList(new TypeReference<Address>(true) {}, new TypeReference<Address>(true) {}, new TypeReference<Uint256>(true) {}));
-        
-        filter.addSingleTopic(EventEncoder.encode(transferEvent));
-
-        web3.ethLogFlowable(filter).subscribe(eventLog -> {
-//        	String smartContractAddress = eventLog.getTopics().get(0);
-//        	String from = eventLog.getTopics().get(1);
-        	String to = eventLog.getTopics().get(2);
-        	String tokenId = eventLog.getTopics().get(3);
-        	System.out.println("New Owner :: 0x" + Keys.toChecksumAddress(to).substring(26) + "  &  TokenId :: " + Long.decode(tokenId));
-        	
-        }, e -> {
-        	if (e instanceof ArrayIndexOutOfBoundsException) {
-//        	 TODO catch 
-                e.printStackTrace();
-            } else if (e instanceof NumberFormatException ) {
-//        	 TODO catch 
-                e.printStackTrace();
-            } else {
-                e.printStackTrace();
+		try (Scanner scanner = new Scanner(System.in);){
+			
+//			Set Infura Endpoint & Contract Address
+        	System.out.println("Enter Your Infura WSS Endpoint (  wss://{} ) : ");
+    		String infuraEndpoint = scanner.nextLine();
+    		System.out.println("Enter Your Smart Contract Address : ");
+    		String contractAddress = scanner.nextLine();
+    		
+//			Try Socket Connect 
+            WebSocketService webSocketService = new WebSocketService(infuraEndpoint, true);
+            System.out.println("== Socket Connecting Started ==");
+            int failCnt = 5;
+            while (failCnt >= 0) {
+            	try {
+        			webSocketService.connect();
+        			break;
+        		} catch (ConnectException e1) {
+        			if(failCnt == 0) e1.printStackTrace();
+        			failCnt--;
+        		}
             }
-        });
+            System.out.println("== Socket Connected ==");
+            
+//			The schedule for checking the Web3 Client version has been set to maintain the socket connection.
+            Timer timer = new Timer();
+            TimerTask timerTask = new TimerTask() {
+    			@Override
+    			public void run() {
+    				Request<?, Web3ClientVersion> request = new Request<>("web3_clientVersion", Arrays.asList(), webSocketService, Web3ClientVersion.class);
+    				webSocketService.sendAsync(request, Web3ClientVersion.class).thenAccept(v -> {
+    					LocalDateTime now = LocalDateTime.now();
+    					System.out.println(String.format("[ %s ] [Check Version] - %s", now.toString() ,v.getResult()));
+    				});
+    			}
+    		};
+    		timer.scheduleAtFixedRate(timerTask, 0, 3000000);
+            
+//			Event and filter setup for subscribing to Transfer.
+// 			To examine the transfer logs of all blocks from the initial execution, set the filter's block range from EARLIEST to LATEST.
+
+            Web3j web3 = Web3j.build(webSocketService);  
+            EthFilter filter = new EthFilter(
+                    DefaultBlockParameterName.LATEST,
+                    DefaultBlockParameterName.LATEST,
+                    contractAddress
+            );
+            Event TRANSFER_EVENT = new Event("Transfer", 
+            	    Arrays.<TypeReference<?>>asList(new TypeReference<Address>(true) {}, new TypeReference<Address>(true) {}, new TypeReference<Uint256>(true) {}));
+            
+//			Add Transfer Event Topic To Filter
+            filter.addSingleTopic(EventEncoder.encode(TRANSFER_EVENT));
+
+//			Event Subscribe
+            web3.ethLogFlowable(filter).subscribe(eventLog -> {
+//				Event Log analysis
+				
+//            	String smartContractAddress = eventLog.getTopics().get(0);
+//            	String from = eventLog.getTopics().get(1);
+            	String to = eventLog.getTopics().get(2);
+            	String tokenId = eventLog.getTopics().get(3);
+            	LocalDateTime now = LocalDateTime.now();
+            	System.out.println("New Owner :: 0x" + Keys.toChecksumAddress(to).substring(26) + "  &  TokenId :: " + Long.decode(tokenId) + " :: " + now);          
+            }, e -> {
+//				TODO
+            	if (e instanceof ArrayIndexOutOfBoundsException) {
+                    e.printStackTrace();
+                } else if (e instanceof NumberFormatException ) {
+                    e.printStackTrace();
+                } else {
+                    e.printStackTrace();
+                }
+            });
+        } catch {
+//			TODO
+		}
+
     }
 
 
